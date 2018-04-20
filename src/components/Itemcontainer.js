4946cd0399588aa4580fda691be56c3c307d67d8
Steve Sloan <steve@stevesloan.ca>
2018-04-20 10:55:22 -0400

    Itemcontainer: add keys to each item in list

--- src/components/Itemcontainer.js
+++ src/components/Itemcontainer.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 export default class Itemcontainer extends Component {
-  render() {
-    const items = this.props.shoppingList.items.map(list => {
+    render() {
+        const items = this.props.shoppingList.items.map((list, key) => {
+            return (
+                <ListGroupItem key={key}>{list.name}</ListGroupItem>
+            )
+        });
         return (
-            <ListGroupItem>{list.name}</ListGroupItem>
+            <div>
+                <h1>{this.props.shoppingList.name}</h1>
+                <ListGroup>{items}</ListGroup>
+            </div>
         )
-    });
-    return (
-      <div>
-        <h1>{this.props.shoppingList.name}</h1>
-        <ListGroupItem>{items}</ListGroupItem>
-      </div>
-    )
-  }
+    }
 }
